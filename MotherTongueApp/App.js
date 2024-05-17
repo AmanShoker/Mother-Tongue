4ff2e71a8efc380eb3552fb00e807715a4cee508
Kabirgill13 <amanshoker04@gmail.com>
2024-05-17 16:20:27 -0400

    banner

--- MotherTongueApp/App.js
+++ MotherTongueApp/App.js
@@ -3,15 +3,30 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/HomeScreen';
+import { BackHandler } from 'react-native';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={headerStyle}
+      >
         <Stack.Screen name="Home" component={HomeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+const headerStyle = {
+  headerTintColor: '#fff',
+  headerStyle: {
+    backgroundColor: '#333',
+    borderBottomWidth: 0, 
+    height: 0,
+  }, 
+  headerTitleStyle: {
+    fontSize: 0,
+  },
+};
